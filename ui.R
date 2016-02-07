5099d2c5f1dbac337a554f58a43ccbdfea91242d
Ilari Scheinin <ilari.scheinin@gmail.com>
2016-02-07 14:39:06 +0200

    Add link to blog entry

--- ui.R
+++ ui.R
@@ -17,15 +17,18 @@ shinyUI(fillPage(title="Plows from HEL",
         min=0L, max=24L, value=c(0L, 3L), step=1L, round=TRUE)),
       column(3L, p(style="font-size: smaller;",
         "This app shows the recent activity of snowplows in Helsinki.",
-        "Made by Ilari Scheinin, source code on",
-        a(href="https://github.com/ilarischeinin/plows-from-hel", "GitHub."),
-        "Data comes from", a(href="http://www.hel.fi/stara", "Stara's"),
+        "Data comes from",
+        a(href="http://www.hel.fi/hki/Rakpa/en/Etusivu", "Stara's"),
         a(href="http://dev.hel.fi/apis/snowplows/", "API,"),
         "which contains only a subset of all plows in use.",
-        "See also Sampsa Kuronen's",
+        a(href="http://ilari.scheinin.fi/snowplows-of-helsinki/", "The idea"),
+        "of this version is to unambiguosly show what activities",
+        "have been peformed along a specific route, whereas Sampsa Kuronen's",
         a(href="http://www.auratkartalla.com", "Aurat kartalla"),
-        "for a prettier overview."
-        , br(), "Data updated: ", textOutput("stamp", inline=TRUE)
+        "provides a prettier overview, and with better performance.",
+        "Made by Ilari Scheinin, source code on",
+        a(href="https://github.com/ilarischeinin/plows-from-hel", "GitHub."),
+        br(), "Data updated: ", textOutput("stamp", inline=TRUE)
       )),
       div(style="clear: both;")
     )
